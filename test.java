343052ffa53c4a8c78a4b75da15a8993d4a2280b
Prathica Shetty M <123286880+PrathicaShettyM@users.noreply.github.com>
2024-04-05 19:19:08 +0530

    update

--- test.java
+++ test.java
@@ -1,18 +1,48 @@
 public class test {
     public static void main(String[] args) {
-        int[] nums = {1,2};
-        int mx = Integer.MIN_VALUE, mn = Integer.MAX_VALUE;
-        for(int i  = 0; i < nums.length; i++){
-            mx = Math.max(mx, nums[i]);
-            mn = Math.max(mn, nums[i]);
+        int[][] arr = {{1,0,0},{0,0,1},{1,0,0}};
+        int[] row = new int[3];
+        int[] col = new int[3];
+        int count;
+        // count 1s in rows
+        for(int i = 0; i < 3; i++){
+            count = 0;
+            for(int j = 0; j < 3; j++){
+                if(arr[i][j] == 1){
+                    count += 1;   
+                }
+                row[i] = count;
+            }
         }
-        int count = 0;
-        System.out.println(mn);
-        System.out.println(mx);
-        // for(int i = 0; i < nums.length; i++){
-        //     if(nums[i]!= mn && nums[i]!= mx) 
-        //         count+=1;
-        // }
         
+        for(int i = 0; i < 3; i++){
+            System.out.print(row[i] + " ");
+        }
+
+        for(int j = 0; j < 3; j++){
+            count = 0;
+            for(int i = 0; i < 3; i++){
+                if(arr[i][j] == 1){
+                    count += 1;
+                }
+                col[j] = count;
+            }
+        }
+        System.out.println();
+        for(int i = 0; i < 3; i++){
+            System.out.print(col[i] + " ");
+        }
+
+        int sum = 0;
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                if(row[i]==1 && col[j]==1){
+                    sum+=1;
+                    
+                }
+            }
+        }
+        System.out.println();
+        System.out.println(sum);
     }
 }
